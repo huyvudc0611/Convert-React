4d97faf395411ee92fb8c3df89f9bb3f9d3c5793
vu huy <you@example.com>
2020-04-28 21:08:04 +0700

    Updates

---
+++ precache-manifest.a34478aabeb1d4bbcfcbe65ef164271a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01731d7b5012a775d7363632d52b66d6",
+    "url": "/Convert-React/index.html"
+  },
+  {
+    "revision": "aaf6d093d8ab5c8a013e",
+    "url": "/Convert-React/static/css/main.7357feda.chunk.css"
+  },
+  {
+    "revision": "67a6a1160864da33e0f9",
+    "url": "/Convert-React/static/js/2.a98eab19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Convert-React/static/js/2.a98eab19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf6d093d8ab5c8a013e",
+    "url": "/Convert-React/static/js/main.521f5091.chunk.js"
+  },
+  {
+    "revision": "7f3841ec4fa4d49783e9",
+    "url": "/Convert-React/static/js/runtime-main.e4927e6d.js"
+  }
+]);
